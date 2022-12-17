e9083c50a1cb0a55828a5c1a801386c3a19bbe31
Alejandro Gomez <73868258+agosmou@users.noreply.github.com>
2022-12-17 15:13:14 -0500

    added client.py

--- server/db/client.py
+++ server/db/client.py
@@ -1,17 +1,35 @@
-# dummy client
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from server.db.schema import Prompt, Response
+
 
 class DbClient():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, address) -> None:
+        # self.conn=create_engine(address) # creates the connection
+        self.session = Session(create_engine(address))
 
     def getPrompts(self):
-        pass
+        return self.session.query(Prompt).all()
 
     def insertPrompt(self, query):
-        pass
+        prompt = Prompt(
+            id="",
+            query=query,
+            createdAt=123
+        )
+        self.session.add(prompt)
+        self.session.commit()
 
     def getResponses(self, promptId):
-        pass
+        return self.session.query(Response).filter_by(promptId=promptId)
 
     def insertResponse(self, promptId, content, sentiment):
-        pass
+        response = Response(
+            id="",
+            promptId=promptId,
+            content=content,
+            sentiment=sentiment,
+            createdAt=123
+        )
+        self.session.add(response)
+        self.session.commit()
