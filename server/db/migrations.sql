8d31de9c684d9185366a34c83894e65ea47d097c
Alejandro Gomez <73868258+agosmou@users.noreply.github.com>
2022-12-17 15:52:54 -0500

    fixed migrations.sql prompts query

--- server/db/migrations.sql
+++ server/db/migrations.sql
@@ -1,8 +1,8 @@
 -- ale: create prompts tb
-CREATE TABLE IF NOT EXISTS responses (
+CREATE TABLE IF NOT EXISTS prompts (
     id VARCHAR(255) PRIMARY KEY,
     query TEXT NOT NULL,
-    createdAt BIGINT NOT NULL,
+    createdAt BIGINT NOT NULL
 );
 
 
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS responses (
   promptId VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
   sentiment VARCHAR(10) NOT NULL,
-  createdAt BIGINT NOT NULL,
+  createdAt BIGINT NOT NULL
 );
 
 
