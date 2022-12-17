c5be15a777d0431fc1f168e7692b9bb54b02ae73
JObog <jobogbaimhe@gmail.com>
2022-12-17 13:14:51 -0500

    updating env example file

--- server/clients/cohere.py
+++ server/clients/cohere.py
@@ -4,14 +4,14 @@ DEFAULT_MODEL_SIZE = "medium"
 
 
 class CohereClient:
-    def __init__(self, apiKey) -> None:
+    def __init__(self, apiKey):
         self.client = cohere.Client(apiKey)
 
-    def get_score(self, exampleResponses, actualResponse):
+    def get_score(self, example_responses, actual_response):
         response = self.client.classify(
             model=DEFAULT_MODEL_SIZE,
-            inputs=[actualResponse],
-            examples=exampleResponses,
+            inputs=[actual_response],
+            examples=example_responses,
         )
 
         result = response.classifications[0]
