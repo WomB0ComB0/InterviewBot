47b43cbe0517301ca44c21f62d7ff6cbe3d40446
JObog <jobogbaimhe@gmail.com>
2022-12-17 22:42:20 -0500

    injecting db client into resource

--- server/resources/interview.py
+++ server/resources/interview.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, jsonify, Response
 from server.cohere.client import CohereClient
+from server.db.client import DbClient
 
 # resource to handle the endpoints related to interviewing
 class InterviewResource(Blueprint):
-    def __init__(self, cohere_client: CohereClient):
+    def __init__(self, cohere_client: CohereClient, db_client: DbClient):
         super().__init__("interview", __name__)
         self.cohere_client = cohere_client
+        self.db_client = db_client
+        # configuring routes
         self.add_url_rule("/prompts", view_func=self.get_prompts, methods=["GET"])
         self.add_url_rule("/answer", view_func=self.answer_prompt, methods=["POST"])
 
-    # TODO: implement
     def get_prompts(self) -> Response:
         return jsonify(prompts=["prompt1", "prompt2", "prompt3"])
 
